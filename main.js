c9b2c2353eadec404f6e47eff7bc05914bbd5ca0
lehandro <lehandro@protonmail.ch>
2017-04-05 19:51:48 -0400

    'javascript2'

---
+++ main.js
@@ -0,0 +1,9 @@
+
+var num1 = prompt("Ingresar Primer Número")
+var num2 = prompt("Ingresar Segundo Número")
+var sumar = Number(num1)+Number(num2)
+var rest = Number(num1)-Number(num2)
+var mult = Number(num1)*Number(num2)
+var div = Number(num1)/Number(num2)
+console.log("sumar", "rest", "mult", "div")
+alert("el resultado suma " + sumar + "el resultado resta " + rest + "el resultado multiplicación " + mult + "el resultado divición " + div + "Total de resultado" )
